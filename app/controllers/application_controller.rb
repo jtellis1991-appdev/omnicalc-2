bc57373da25a12ff6677e283c916f67ede7448cd
jtellis1991 <joshuaellis1991@gmail.com>
2021-02-01 23:03:18 +0000

    Complete wizard subtract

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,20 @@ class ApplicationController < ActionController::Base
     render( {:template => "math_templates/add.html.erb"})
   end
 
+  def subtract_results
+
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @difference = @second - @first
+
+
+    render( {:template => "math_templates/wizard_subtract.html.erb"})
+  end
+
+  def subtraction_for_humans
+
+    render( {:template => "math_templates/subtract.html.erb"})
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get("/wizard_add", { :controller => "application", :action => "add_results"})
 
-  
+  get("/subtract", { :controller => "application", :action => "subtraction_for_humans"})
+
+   get("/wizard_subtract", { :controller => "application", :action => "subtract_results"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
