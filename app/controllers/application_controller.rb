cc45022c2f3d0341ee9ce88cad8aec462984da68
jtellis1991 <joshuaellis1991@gmail.com>
2021-02-01 22:53:53 +0000

    complete add form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  def add_results
+
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @sum = @first + @second
+
+
+    render( {:template => "math_templates/wizard_add.html.erb"})
+  end
+
+    def addition_for_humans
+
+    render( {:template => "math_templates/add.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
+  get("/add", { :controller => "application", :action => "addition_for_humans"})
+
+  get("/wizard_add", { :controller => "application", :action => "add_results"})
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
